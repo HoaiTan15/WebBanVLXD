fbd949c62d1f6e850f57c0a2c2f4c07fb4199616
HoaiTan15 <tanphan1501@gmail.com>
2025-11-10 10:21:15 +0700

    Add project files.

---
+++ WebBanVLXD/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanVLXD.Models;
+
+namespace WebBanVLXD.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        VLXD_DBEntities db = new VLXD_DBEntities();
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string TenDangNhap, string MatKhau)
+        {
+            var user = db.TaiKhoans.FirstOrDefault(u => u.TenDangNhap == TenDangNhap && u.MatKhau == MatKhau && u.TrangThai == true);
+            if (user != null)
+            {
+                // Lưu session đăng nhập
+                Session["UserID"] = user.TaiKhoanID;
+                Session["UserName"] = user.TenDangNhap;
+                Session["Role"] = user.PhanQuyen.TenQuyen;
+
+                // Chuyển hướng theo quyền
+                if (user.PhanQuyen.TenQuyen == "Admin")
+                    return RedirectToAction("TrangChu", "Home");
+                else if (user.PhanQuyen.TenQuyen == "NhanVien")
+                    return RedirectToAction("Index", "SanPham");
+                else
+                    return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Sai tên đăng nhập hoặc mật khẩu!";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+        
+    }
+}
---
+++ WebBanVLXD/Models/QuenMatKhau.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBanVLXD.Models
+{
+    public class QuenMatKhau
+    {
+        [Required(ErrorMessage = "Email là bắt buộc")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; }
+    }
+    public class DoiMatKhau
+    {
+        [Required(ErrorMessage = "Mật khẩu mới là bắt buộc")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        public string MatKhauMoi { get; set; }
+        [Required(ErrorMessage = "Xác nhận mật khẩu là bắt buộc")]
+        [DataType(DataType.Password)]
+        [Compare("MatKhauMoi", ErrorMessage = "Mật khẩu xác nhận không khớp")]
+        public string XacNhanMatKhau { get; set; }
+        public string Token { get; set; }
+    }
+    public class ResetMatKhauModel
+    {
+        [Required(ErrorMessage = "Mật khẩu mới là bắt buộc")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        public string MatKhauMoi { get; set; }
+        [Required(ErrorMessage = "Xác nhận mật khẩu là bắt buộc")]
+        [DataType(DataType.Password)]
+        [Compare("MatKhauMoi", ErrorMessage = "Mật khẩu xác nhận không khớp")]
+        public string XacNhanMatKhau { get; set; }
+    }
+}
